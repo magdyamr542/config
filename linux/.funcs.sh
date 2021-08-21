2314feca4c07abf490cb654c7ac42cb8627005ee
Amr Metwally <amr.metwally@semasquare.com>
2021-08-21 18:39:19 +0200

    added cpfile command using the xsel clipboard

--- linux/.funcs.sh
+++ linux/.funcs.sh
@@ -9,3 +9,21 @@ gbname () {
   git branch  | grep "$1"
 }
 
+
+# check if command exists
+doesCommandExist () {
+  return $(command -v $1 &> /dev/null)
+}
+
+
+# easy cp of file
+cpfile () {
+  if  doesCommandExist xsel
+    then
+        file=$(realpath $1)
+        cat $file | xsel -b 
+    else
+      echo "xsel is not installed. please install it first..."
+  fi
+
+}
