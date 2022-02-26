1a23585ad2dcc9b4bee61422aad813b7f50d9528
amr.metwally <magdyamr542@gmail.com>
2022-02-26 19:24:01 +0100

    added glc mac

--- mac/.aliases.sh
+++ mac/.aliases.sh
@@ -12,6 +12,8 @@ alias fzf="fzf --preview 'bat --style=numbers --color=always --line-range :500 {
 alias genc="/usr/local/Cellar/genact/0.11.0/bin/genact"
 alias vim="nvim"
 
+
 # Git
 alias gpl="git pull"
 alias gcm="git commit -m"
+alias glc='git add .; git commit --amend --no-edit' # add changes to last commit (glc git last commit)
