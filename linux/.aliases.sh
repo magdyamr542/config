1f508ce09a2e65ef4c2722cba3549512d6a93abb
Amr Metwally <amr.metwally@green-connector.de>
2022-12-03 20:29:58 +0100

    Added git log alias

--- linux/.aliases.sh
+++ linux/.aliases.sh
@@ -13,6 +13,7 @@ alias t='tree'
 alias gcm='git commit -m'
 alias gpl='git pull'
 alias glc='git add .; git commit --amend --no-edit' # add changes to last commit (glc git last commit)
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 
 #Vim
 alias vim="nvim"
